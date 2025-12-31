4b6da589049fe48c1f41160283e0d29451f6e9a9
oussamasine07 <sineoussama@gmail.com>
2025-12-31 16:56:55 +0100

    add classroom model

---
+++ backend/school-service/src/main/java/org/schoolservice/model/ClassRoom.java
@@ -0,0 +1,31 @@
+package org.schoolservice.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "class_rooms")
+public class ClassRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
