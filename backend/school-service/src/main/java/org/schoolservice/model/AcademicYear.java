1a8d630e2e5685e485b414143a4318ee54ee9225
oussamasine07 <sineoussama@gmail.com>
2026-01-07 17:28:54 +0100

    finish academic year CRUD

--- backend/school-service/src/main/java/org/schoolservice/model/AcademicYear.java
+++ backend/school-service/src/main/java/org/schoolservice/model/AcademicYear.java
@@ -1,23 +1,56 @@
 package org.schoolservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "academic_years")
 public class AcademicYear {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "start_date")
     private LocalDate start;
+
+    @Column(name = "end_date")
     private LocalDate end;
+    @Column(name = "is_closed")
     private boolean isClosed;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDate start) {
+        this.start = start;
+    }
+
+    public LocalDate getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDate end) {
+        this.end = end;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
+    }
+
+    public void setClosed(boolean closed) {
+        isClosed = closed;
+    }
 }
 
 
---
+++ backend/school-service/src/main/java/org/schoolservice/repo/AcademicYearRepo.java
@@ -0,0 +1,8 @@
+package org.schoolservice.repo;
+
+import org.schoolservice.model.AcademicYear;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcademicYearRepo extends JpaRepository<AcademicYear, Long> {
+
+}
